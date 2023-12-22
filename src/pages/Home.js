5c3192374a74c2f8d2a9023ddb7c483d2c0dc01e
DiegoGCaullan <diegoo_2058@hotmail.com>
2023-12-21 21:07:39 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const Home = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return user ? (
+    <>
+      <div className="mx-2 d-flex  flex-column align-items-center">
+        <h3>
+          Home - Bienvenido{" "}
+          <span className="text-primary">
+            {user.firstname + " " + user.lastname}
+          </span>
+        </h3>
+        <button className="btn btn-danger w-50 mt-3" onClick={logout}>
+          Cerrar Sesion
+        </button>
+      </div>
+    </>
+  ) : null;
+};
+
+export default Home;
